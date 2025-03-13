6fadbd84e8f170a53b053d8e5bb265280208849d
Adrian <adrian.ciolkiewicz@makimo.pl>
2025-03-13 20:21:16 +0100

    feat: create api server and add first endpoint

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription API");
+});
+
+app.listen(3000, () => {
+  console.log("Subscription API is running on http://localhost:3000");
+});
+
+export default app;
