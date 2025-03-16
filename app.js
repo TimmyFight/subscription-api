42af4d5d4d56c69725edede2639e8021cbada00b
Adrian <adrian.ciolkiewicz@makimo.pl>
2025-03-16 22:05:52 +0100

    feat: add routes like users, subscriptions or auth

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { PORT } from "./config/env.js";
 
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 app.get("/", (req, res) => {
-  res.send("Welcome to the Subscription API");
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Subscription API is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
 });
 
 export default app;
