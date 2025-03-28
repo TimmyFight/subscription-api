4b56727e65d07d8a144caf10c22a852091e4f8d1
Adrian <adrian.ciolkiewicz@makimo.pl>
2025-03-28 15:12:18 +0100

    feat: implement arcject to protect api and prevent bot and attacks

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjectMiddleware from "./middlewares/arcject.middleware.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjectMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
